bb046fd23ce7afbe2e209032c23d04018f1bc75b
haloperidozz <196709604+haloperidozz@users.noreply.github.com>
2025-02-04 18:27:06 +0600

    refactor(common): enhance modify for specific generator types

--- compose-app/src/commonMain/kotlin/com/github/haloperidozz/obfuscator/generator/TextGeneratorModify.kt
+++ compose-app/src/commonMain/kotlin/com/github/haloperidozz/obfuscator/generator/TextGeneratorModify.kt
@@ -1,7 +1,47 @@
 package com.github.haloperidozz.obfuscator.generator
 
-fun <T> TextGenerator<T>.modify(
-    customGenerate: (generator: TextGenerator<T>, input: String, value: T) -> String
-): TextGenerator<T> {
-    return TextGenerator { input, value -> customGenerate(this@modify, input, value) }
+inline fun FloatTextGenerator.modify(
+    crossinline customGenerate: (
+        generator: TextGenerator<Float>, input: String, value: Float
+    ) -> String
+) = object : FloatTextGenerator() {
+    override val meta: TextGeneratorMeta = this@modify.meta
+
+    override fun doGenerate(input: String, value: Float): String {
+        return customGenerate(this@modify, input, value)
+    }
+}
+
+inline fun SelectTextGenerator.modify(
+    crossinline customGenerate: (
+        generator: TextGenerator<Int>, input: String, selected: String, index: Int
+    ) -> String
+) = object : SelectTextGenerator() {
+    override val meta: TextGeneratorMeta = this@modify.meta
+
+    override fun generate(input: String, selected: String, index: Int): String {
+        return customGenerate(this@modify, input, selected, index)
+    }
+}
+
+inline fun SimpleTextGenerator.modify(
+    crossinline customGenerate: (generator: TextGenerator<Unit>, input: String) -> String
+) = object : SimpleTextGenerator() {
+    override val meta: TextGeneratorMeta = this@modify.meta
+
+    override fun generate(input: String): String {
+        return customGenerate(this@modify, input)
+    }
+}
+
+inline fun StringTextGenerator.modify(
+    crossinline customGenerate: (
+        generator: TextGenerator<String>, input: String, value: String
+    ) -> String
+) = object : StringTextGenerator {
+    override val meta: TextGeneratorMeta = this@modify.meta
+
+    override fun generate(input: String, value: String): String {
+        return customGenerate(this@modify, input, value)
+    }
 }
