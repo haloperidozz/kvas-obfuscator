cee12be3a6b03a689e9bbf43082317839c920d80
haloperidozz <196709604+haloperidozz@users.noreply.github.com>
2025-02-06 18:10:26 +0600

    docs(common): add warning and usage instructions for TextGenerator

--- compose-app/src/commonMain/kotlin/com/github/haloperidozz/obfuscator/generator/TextGenerator.kt
+++ compose-app/src/commonMain/kotlin/com/github/haloperidozz/obfuscator/generator/TextGenerator.kt
@@ -1,5 +1,14 @@
 package com.github.haloperidozz.obfuscator.generator
 
+/**
+ * WARNING: Do **NOT** implement this interface directly.
+ *
+ * Instead, use one of the provided implementations:
+ * - [com.github.haloperidozz.obfuscator.generator.type.FloatTextGenerator]
+ * - [com.github.haloperidozz.obfuscator.generator.type.SelectTextGenerator]
+ * - [com.github.haloperidozz.obfuscator.generator.type.SimpleTextGenerator]
+ * - [com.github.haloperidozz.obfuscator.generator.type.StringTextGenerator]
+ */
 interface TextGenerator<T> {
     fun generate(input: String, value: T): String
 }
