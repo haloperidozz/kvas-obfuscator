1afe68ef39e08bbdd597a3c87821b4099bf337fe
haloperidozz <196709604+haloperidozz@users.noreply.github.com>
2025-02-08 00:46:11 +0600

    refactor(common): remove favorite text generator repository

--- compose-app/src/commonMain/kotlin/com/github/haloperidozz/obfuscator/di/RepositoryModule.kt
+++ compose-app/src/commonMain/kotlin/com/github/haloperidozz/obfuscator/di/RepositoryModule.kt
@@ -16,15 +16,10 @@
  */
 package com.github.haloperidozz.obfuscator.di
 
-import com.github.haloperidozz.obfuscator.data.favorite.FavoriteTextGeneratorRepositoryImpl
 import com.github.haloperidozz.obfuscator.data.generator.TextGeneratorRepositoryImpl
-import com.github.haloperidozz.obfuscator.repository.FavoriteTextGeneratorRepository
 import com.github.haloperidozz.obfuscator.repository.TextGeneratorRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
     single<TextGeneratorRepository> { TextGeneratorRepositoryImpl() }
-    single<FavoriteTextGeneratorRepository> {
-        FavoriteTextGeneratorRepositoryImpl(get(), get())
-    }
 }
